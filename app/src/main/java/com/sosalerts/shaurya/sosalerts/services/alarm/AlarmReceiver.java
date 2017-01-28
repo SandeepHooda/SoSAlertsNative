125937ca3196626d8afb4d2113a9e0adb80e43d3
SandeepHooda <sonu.hooda@gmail.com>
2017-01-28 17:49:24 +0530

    Get Location

--- app/src/main/java/com/sosalerts/shaurya/sosalerts/services/alarm/AlarmReceiver.java
+++ app/src/main/java/com/sosalerts/shaurya/sosalerts/services/alarm/AlarmReceiver.java
@@ -3,13 +3,10 @@ package com.sosalerts.shaurya.sosalerts.services.alarm;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.speech.tts.TextToSpeech;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.sosalerts.shaurya.sosalerts.MainActivity;
-import com.sosalerts.shaurya.sosalerts.services.sms.ReadOut;
-import com.sosalerts.shaurya.sosalerts.services.util.GetLocationCordinatesActivity;
+import com.sosalerts.shaurya.sosalerts.services.util.GetLocationCordinatesService;
 
 /**
  * Created by shaurya on 1/26/2017.
@@ -17,14 +14,15 @@ import com.sosalerts.shaurya.sosalerts.services.util.GetLocationCordinatesActivi
 
 public class AlarmReceiver extends BroadcastReceiver {
     private final String fileName = this.getClass().getSimpleName();
+    public static final String originator = "AlarmReceiver";
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.e(fileName, "Alarm service With address ");
 
-        /*Intent speakIntent = new Intent(context, ReadOut.class);
-        speakIntent.putExtra(MainActivity.textToSpeak,"Fire");
-        context.startService(speakIntent);*/
-        Intent locationCordinatesIntent = new Intent(context, GetLocationCordinatesActivity.class);
+
+        Intent locationCordinatesIntent = new Intent(context, GetLocationCordinatesService.class);
+        locationCordinatesIntent.putExtra(MainActivity.orignationActivityName, originator+(Math.random()));
+        //locationCordinatesIntent.putExtra(GetLocationCordinatesService.ADDRESS_RESULT_RECEIVER, new LocationTrackerIntentService());
         context.startService(locationCordinatesIntent);
 
     }
